acdbb363c296a4fc6f541ae574e0cbc6841a1650
Tony Onodi <tony@Tonys-MacBook-Pro.local>
2014-03-08 23:39:36 +0000

    Some code tidying and working, minified version added.

--- js/class-cycle.js
+++ js/class-cycle.js
@@ -1,14 +1,13 @@
 // Function to gets all elements with a certain attribute.
 function getElementsWithAtt( attribute ) {
 	// Array is to be returned
-	var matchingElements = []
-	// get all elements in document
-	var allElements = document.getElementsByTagName("*");
+	var matchingElements = [],
+		allElements = document.getElementsByTagName("*");  // get all elements in document
 
 	// Loop over all elements
 	for(var i = 0; i < allElements.length; i++) {
 		
-		var hasAttribute = allElements[i].getAttribute("data-cycle-list");
+		var hasAttribute = allElements[i].getAttribute( attribute );
 		
 		// push to array if attribute exists
 		if ( hasAttribute ) {
@@ -20,26 +19,23 @@ function getElementsWithAtt( attribute ) {
 }
 
 function processCycle( element ) {
-	// get all top level children of element
-	var cycleChildren = element.childNodes;
-	// get list of classes to cycle through
-	var cycleListString = element.getAttribute("data-cycle-list");
-	// Arrayify the list
-	var cycleListArray = cycleListString.split(" ");
-	var listLength = cycleListArray.length;
+	var cycleChildren = element.children,	 // get all top level children of element
+		cycleListString = element.getAttribute( "data-cycle-list" ),	// get list of classes to cycle through
+		cycleListArray = cycleListString.split( " " ),	// Arrayify the list
+		listLength = cycleListArray.length;
 
 	// Loop over all child elements and append appropriate class.
 	for ( var i = 0; i < cycleChildren.length; i++ ) {		
-		var modulo = i % listLength;
-		var newClass = " " + cycleListArray[modulo];
+		var modulo = i % listLength,
+			newClass = " " + cycleListArray[ modulo ];
 
 		// Append class.
-		cycleChildren[i].class += newClass;
+		cycleChildren[i].className += newClass;
 	}
 }
 
 // get all elements with data-cycle-list attributes
-var classCycleElements = getElementsWithAtt("data-cycle-list");
+var classCycleElements = getElementsWithAtt( "data-cycle-list" );
 
 // Loops over all relevant elements and passes them to the main processCycle function
 if (classCycleElements) {
