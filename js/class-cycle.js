fa0e874a7f6c5e5c34980ad9c675ebf209e68767
Tony Onodi <tony@Tonys-MacBook-Pro.local>
2014-03-08 21:45:16 +0000

    Process cycle finished, needs to be tested.

--- js/class-cycle.js
+++ js/class-cycle.js
@@ -1,14 +1,14 @@
 // Function to gets all elements with a certain attribute.
-function getElementsWithAtt(attribute) {
+function getElementsWithAtt( attribute ) {
 	// Array is to be returned
-	matchingElements = []
+	var matchingElements = []
 	// get all elements in document
-	allElements = document.getElementsByTagName("*");
+	var allElements = document.getElementsByTagName("*");
 
 	// Loop over all elements
 	for(var i = 0; i < allElements.length; i++) {
 		
-		hasAttribute = allElements[i].getAttribute();
+		var hasAttribute = allElements[i].getAttribute();
 		
 		// push to array if attribute exists
 		if ( hasAttribute ) {
@@ -19,9 +19,29 @@ function getElementsWithAtt(attribute) {
 	return matchingElements;
 }
 
+function processCycle( element ) {
+	// get all top level children of element
+	var cycleChildren = element.childNodes;
+	// get list of classes to cycle through
+	var cycleListString = element.getAttribute("data-cycle-list");
+	// Arrayify the list
+	var cycleListArray = cycleListString.split(" ");
+	var listLength = cycleListArray.length;
+
+	// Loop over all child elements and append appropriate class.
+	for ( var i = 0; i < cycleChildren.length; i++ ) {		
+		var modulo = i % listLength;
+		var newClass = " " + cycleList[modulo];
+
+		// Append class.
+		cycleChildren[i].class += newClass;
+	}
+}
+
 // get all elements with data-cycle-list attributes
 var classCycleElements = getElementsWithAtt("data-cycle-list");
 
+// Loops over all relevant elements and passes them to the main processCycle function
 if (classCycleElements) {
 	for ( var i = 0; i < classCycleElements.length; i++ ) {
 		processCycle( classCycleElements[i] );
